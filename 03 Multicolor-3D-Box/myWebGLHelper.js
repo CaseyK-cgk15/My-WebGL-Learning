2026c43c5630e15595a27e56e578025c5e49210a
Casey Koepp <cgk15@uakron.edu>
2021-03-15 20:25:35 -0400

    Update WebGLHelper to 1.1

--- 03 Multicolor-3D-Box/myWebGLHelper.js
+++ 03 Multicolor-3D-Box/myWebGLHelper.js
@@ -1,7 +1,19 @@
 // myWebGLHelper.js
+// version 1.1
+// author Casey Koepp
 // gl = WebGLRenderingContext object
 
+/**
+ * *********************************************
+ * ---= Shaders & WebGL Program =---
+ * *********************************************
+ */
+
 // createShader()
+// creates vertex or fragment shaders given gl and source code
+// gl is WebGLRenderingContext object
+// type is gl.VERTEX_SHADER or gl.FRAGMENT_SHADER
+// source is source code in form of a string
 function myWebGLHelper_createShader(gl, type, source)
 {
     let shader = gl.createShader(type);
@@ -19,6 +31,8 @@ function myWebGLHelper_createShader(gl, type, source)
 };
 
 // createProgram()
+// creates WebGL program from vertexShader and fragmentShader
+// gl is WebGLRenderingContext object
 function myWebGLHelper_createProgram(gl, vertexShader, fragmentShader)
 {
     let program = gl.createProgram();
@@ -35,3 +49,59 @@ function myWebGLHelper_createProgram(gl, vertexShader, fragmentShader)
     console.log(gl.getProgramInfoLog(program));
     gl.deleteProgram(program);
 };
+
+// createProgramFromSource()
+// runs createShader() and createProgram() and returns program
+// gl is WebGLRenderingContext object
+function myWebGLHelper_createProgramFromSource(gl, vertexSourceCode, fragmentSourceCode)
+{
+    const vertexShader = myWebGLHelper_createShader(gl, gl.VERTEX_SHADER, vertexSourceCode);
+    const fragmentShader = myWebGLHelper_createShader(gl, gl.FRAGMENT_SHADER, fragmentSourceCode);
+    const program = myWebGLHelper_createProgram(gl, vertexShader, fragmentShader);
+
+    return program;
+};
+
+/**
+ * *********************************************
+ *          ---=    Misc.    =---
+ * *********************************************
+ */
+
+// randArr3()
+// returns array of 3 elements with random values
+function myWebGLHelper_randArr3() 
+{ return [Math.random(), Math.random(), Math.random()]; }
+
+// creates array of 6 vertices to make 2 triangles that make a square
+// creates square face in the XY plane
+function myWebGLHelper_squareXY(side)
+{
+    return [
+        // bottom left triangle
+        -side, side, 0,
+        side, -side, 0,
+        -side, -side, 0,
+
+        // top right triangle
+        -side, side, 0,
+        side, -side, 0,
+        side, side, 0,
+    ];
+}
+
+// array of vertices that form triangles that form a cube that spans -1 to 1
+const myWebGLHelper_cubeVertexData = [
+    -1.0,-1.0,-1.0, -1.0,-1.0, 1.0, -1.0, 1.0, 1.0, //1
+    1.0, 1.0,-1.0, -1.0,-1.0,-1.0, -1.0, 1.0,-1.0, //2
+    1.0,-1.0, 1.0, -1.0,-1.0,-1.0, 1.0,-1.0,-1.0, //3
+    1.0, 1.0,-1.0, 1.0,-1.0,-1.0, -1.0,-1.0,-1.0, //4
+    -1.0,-1.0,-1.0, -1.0, 1.0, 1.0, -1.0, 1.0,-1.0, //5
+    1.0,-1.0, 1.0, -1.0,-1.0, 1.0, -1.0,-1.0,-1.0, //6
+    -1.0, 1.0, 1.0, -1.0,-1.0, 1.0, 1.0,-1.0, 1.0, //7
+    1.0, 1.0, 1.0, 1.0,-1.0,-1.0, 1.0, 1.0,-1.0, //8 
+    1.0,-1.0,-1.0, 1.0, 1.0, 1.0, 1.0,-1.0, 1.0, //9
+    1.0, 1.0, 1.0, 1.0, 1.0,-1.0, -1.0, 1.0,-1.0, //10
+    1.0, 1.0, 1.0, -1.0, 1.0,-1.0, -1.0, 1.0, 1.0, //11
+    1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0,-1.0, 1.0 //12
+];
