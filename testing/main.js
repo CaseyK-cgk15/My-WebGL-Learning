308fc34a48a344f055d8b55410c2cbeefc19290d
Casey Koepp <cgk15@uakron.edu>
2021-03-15 23:27:22 -0400

    copy files

---
+++ testing/main.js
@@ -0,0 +1,247 @@
+"use strict";
+
+{
+    const mat4 = glMatrix.mat4;
+
+    // setup Canvas and WebGLRenderingContext (gl)
+    let gl;
+    WebGLSetup();
+
+    /**
+     * *********************************************
+     *     ---= Create Shaders & Program =---
+     * *********************************************
+     */
+
+    // Vertex Shader
+    const vertexShaderSourceCode = 
+    `    
+    precision mediump float;       
+            
+    // an attribute will receive data from a buffer
+    attribute vec4 position;
+    attribute vec4 color;
+    varying vec4 vColor;
+
+    uniform mat4 GLSLmatrix;
+   
+    // all shaders have a main function
+    void main() {
+   
+        vColor = color;
+
+        // gl_Position is a special variable a vertex shader
+        // is responsible for setting
+        gl_Position = GLSLmatrix * position;
+    }
+
+    `
+
+    // Fragment Shader
+    const fragmentShaderSourceCode =
+    `
+
+    // set precision
+    precision mediump float;
+
+    varying vec4 vColor;
+   
+    void main() {
+      // gl_FragColor is a special variable a fragment shader
+      // is responsible for setting
+      gl_FragColor = vColor;
+    }
+   
+    `
+    // create program
+    const program = myWebGLHelper_createProgramFromSource(gl, vertexShaderSourceCode, fragmentShaderSourceCode);
+
+    /**
+     * *********************************************
+     * ---= Buffers, Data, Attribute Locations =---
+     * *********************************************
+     */
+
+    // cube
+    const vertexData = [
+        // Front face
+        -1.0, -1.0,  1.0,
+         1.0, -1.0,  1.0,
+         1.0,  1.0,  1.0,
+        -1.0,  1.0,  1.0,
+      
+        // Back face
+        -1.0, -1.0, -1.0,
+        -1.0,  1.0, -1.0,
+         1.0,  1.0, -1.0,
+         1.0, -1.0, -1.0,
+      
+        // Top face
+        -1.0,  1.0, -1.0,
+        -1.0,  1.0,  1.0,
+         1.0,  1.0,  1.0,
+         1.0,  1.0, -1.0,
+      
+        // Bottom face
+        -1.0, -1.0, -1.0,
+         1.0, -1.0, -1.0,
+         1.0, -1.0,  1.0,
+        -1.0, -1.0,  1.0,
+      
+        // Right face
+         1.0, -1.0, -1.0,
+         1.0,  1.0, -1.0,
+         1.0,  1.0,  1.0,
+         1.0, -1.0,  1.0,
+      
+        // Left face
+        -1.0, -1.0, -1.0,
+        -1.0, -1.0,  1.0,
+        -1.0,  1.0,  1.0,
+        -1.0,  1.0, -1.0,
+      ];
+      
+    const faceColors = [
+        [0.2,  0.2,  0.2,  1.0],    // Front face: grey
+        [1.0,  0.0,  0.0,  1.0],    // Back face: red
+        [0.0,  1.0,  0.0,  1.0],    // Top face: green
+        [0.0,  0.0,  1.0,  1.0],    // Bottom face: blue
+        [1.0,  1.0,  0.0,  1.0],    // Right face: yellow
+        [1.0,  0.0,  1.0,  1.0],    // Left face: purple
+    ];
+
+    // Convert the array of colors into a table for all the vertices.
+
+    let colorData = [];
+
+    for (var j = 0; j < faceColors.length; ++j)
+    {
+        const c = faceColors[j];
+
+        // Repeat each color four times for the four vertices of the face
+        colorData = colorData.concat(c, c, c, c);
+    }
+    
+    const indices = [
+        0,  1,  2,      0,  2,  3,    // front
+        4,  5,  6,      4,  6,  7,    // back
+        8,  9,  10,     8,  10, 11,   // top
+        12, 13, 14,     12, 14, 15,   // bottom
+        16, 17, 18,     16, 18, 19,   // right
+        20, 21, 22,     20, 22, 23,   // left
+    ];
+
+    // get attribute location
+    const positionAttribLocation = gl.getAttribLocation(program, "position");
+    const colorAttribLocation = gl.getAttribLocation(program, "color");
+
+    // create & bind buffer, load data into buffer
+
+    // positionBuffer
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);    
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+    // colorBuffer
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);    
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
+
+    // create the buffer
+    const indexBuffer = gl.createBuffer();
+    // make this buffer the current 'ELEMENT_ARRAY_BUFFER'
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+    // Matrix & Transformations
+    const myMatrix = mat4.create(); // create identity matrix
+    // transformations are done in reverse order of code
+    mat4.scale(myMatrix, myMatrix, [0.4, 0.4, 0.4]);
+    //console.log(myMatrix); // for debugging
+
+    mat4.rotateZ(myMatrix, myMatrix, Math.PI/2 /2);
+    mat4.rotateY(myMatrix, myMatrix, Math.PI/2 /2);
+    mat4.rotateX(myMatrix, myMatrix, Math.PI/2 /2);
+
+    render();
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    function WebGLSetup()
+    {
+        // Setup Canvas and WebGLRenderingContext (gl)
+        var canvas = document.querySelector('canvas');
+        gl = canvas.getContext('webgl');
+        // throw error if WebGL not supported
+        if (!gl) 
+        {
+            alert( "WebGL isn't available" );
+        }
+    };
+
+    function render()
+    {
+        // Animate
+        requestAnimationFrame(render); // parameter is name of function it's in
+
+        /*
+        *   [Resizing Canvas code would go here]
+        */
+
+        // Map clipboard coords to canvas size
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+        // Clear the canvas
+        gl.clearColor(0, 0, 0, 0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        // Execute WebGL Program
+        gl.useProgram(program);
+
+        // attrib pointer vars
+        let vSize = 3;          // 3 components per iteration
+        let cSize = 4;          // 4 components per iteration
+        var type = gl.FLOAT;   // the data is 32bit floats
+        var normalize = false; // don't normalize the data
+        var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+        var offset = 0;        // start at the beginning of the buffer
+
+        // position
+        // Enable Attributes
+        gl.enableVertexAttribArray(positionAttribLocation);
+        // Bind the position buffer.
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer); 
+
+        // bind the buffer containing the indices
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+        // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+        gl.vertexAttribPointer(
+            positionAttribLocation, vSize, type, normalize, stride, offset)
+
+        // color
+        gl.enableVertexAttribArray(colorAttribLocation);
+        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+        gl.vertexAttribPointer(
+            colorAttribLocation, 4, type, normalize, stride, offset)
+
+        // Matrix Locations
+        const uniformLocations =
+        {
+            matrix: gl.getUniformLocation(program, "GLSLmatrix"),
+        };
+        mat4.rotateZ(myMatrix, myMatrix, Math.PI/2 /90);
+        mat4.rotateX(myMatrix, myMatrix, Math.PI/2 /70);
+        gl.uniformMatrix4fv(uniformLocations.matrix, false, myMatrix);
+
+        // Ask WebGL to execute GLSL program
+        var primitiveType = gl.TRIANGLES;
+        var offset = 0;
+        //var count = vertexData.length / 3;
+        var count = 36 // 6 verticies per face * 6 faces
+        gl.enable(gl.DEPTH_TEST);
+        //gl.drawArrays(primitiveType, offset, count);
+        var indexType = gl.UNSIGNED_SHORT;
+        gl.drawElements(primitiveType, count, indexType, offset);
+    };
+}
