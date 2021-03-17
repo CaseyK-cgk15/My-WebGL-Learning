d0a3eab2e5a452f3c53c2304c2361eb530ad5614
Casey Koepp <cgk15@uakron.edu>
2021-03-17 18:27:31 -0400

    Move vertex data into another file

--- testing/main.js
+++ testing/main.js
@@ -62,44 +62,7 @@ const ROTATING = true;
      * *********************************************
      */
 
-    // cube
-    const vertexData = [
-        // Front face
-        -1.0, -1.0,  1.0,
-         1.0, -1.0,  1.0,
-         1.0,  1.0,  1.0,
-        -1.0,  1.0,  1.0,
-      
-        // Back face
-        -1.0, -1.0, -1.0,
-        -1.0,  1.0, -1.0,
-         1.0,  1.0, -1.0,
-         1.0, -1.0, -1.0,
-      
-        // Top face
-        -1.0,  1.0, -1.0,
-        -1.0,  1.0,  1.0,
-         1.0,  1.0,  1.0,
-         1.0,  1.0, -1.0,
-      
-        // Bottom face
-        -1.0, -1.0, -1.0,
-         1.0, -1.0, -1.0,
-         1.0, -1.0,  1.0,
-        -1.0, -1.0,  1.0,
-      
-        // Right face
-         1.0, -1.0, -1.0,
-         1.0,  1.0, -1.0,
-         1.0,  1.0,  1.0,
-         1.0, -1.0,  1.0,
-      
-        // Left face
-        -1.0, -1.0, -1.0,
-        -1.0, -1.0,  1.0,
-        -1.0,  1.0,  1.0,
-        -1.0,  1.0, -1.0,
-      ];
+    const vertexData = myWebGLData.vertexCube01;
       
     const faceColors = [
         [0.2,  0.2,  0.2,  1.0],    // Front face: grey
@@ -122,14 +85,7 @@ const ROTATING = true;
         colorData = colorData.concat(c, c, c, c);
     }
     
-    const indices = [
-        0,  1,  2,      0,  2,  3,    // front
-        4,  5,  6,      4,  6,  7,    // back
-        8,  9,  10,     8,  10, 11,   // top
-        12, 13, 14,     12, 14, 15,   // bottom
-        16, 17, 18,     16, 18, 19,   // right
-        20, 21, 22,     20, 22, 23,   // left
-    ];
+    const indices = myWebGLData.indexCube01;
 
     // get attribute location
     const positionAttribLocation = gl.getAttribLocation(program, "position");
