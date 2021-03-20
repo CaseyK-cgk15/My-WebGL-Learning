c7b969baba501208bbcccf2b89e03da361adafbe
Casey Koepp <cgk15@uakron.edu>
2021-03-20 19:41:44 -0400

    reorganize code, fix mvp matricies & transformations, add canvas resizing function

--- testing/main.js
+++ testing/main.js
@@ -4,7 +4,26 @@ const OPTIONS =
 {
     Rotating: false,
     Isometric_View: true,
-}
+    Animate: false,
+};
+
+function resizeCanvasToDisplaySize(canvas) {
+    // Lookup the size the browser is displaying the canvas in CSS pixels.
+    const displayWidth  = canvas.clientWidth;
+    const displayHeight = canvas.clientHeight;
+   
+    // Check if the canvas is not the same size.
+    const needResize = canvas.width  !== displayWidth ||
+                       canvas.height !== displayHeight;
+   
+    if (needResize) {
+      // Make the canvas the same size
+      canvas.width  = displayWidth;
+      canvas.height = displayHeight;
+    }
+   
+    return needResize;
+  }
 
 {
     const mat4 = glMatrix.mat4;
@@ -25,14 +44,14 @@ const OPTIONS =
     precision mediump float;
             
     // an attribute will receive data from a buffer
-    attribute vec4 a_position;
+    attribute vec3 a_position;
     attribute vec4 a_color;
 
     varying vec4 v_color;
 
     uniform mat4 u_GLSLmatrix;
-    uniform mat4 u_isoMatrix;
-    uniform mat4 u_transMatrix;
+    uniform mat4 u_projectionMatrix;
+    uniform mat4 u_viewMatrix;
    
     // all shaders have a main function
     void main() {
@@ -40,7 +59,7 @@ const OPTIONS =
         v_color = a_color;
 
         // gl_Position is a special variable a vertex shader is responsible for setting
-        gl_Position =  u_isoMatrix * u_transMatrix * u_GLSLmatrix * a_position;
+        gl_Position =  u_projectionMatrix * u_viewMatrix * u_GLSLmatrix * vec4(a_position, 1);
     }
 
     `
@@ -166,28 +185,72 @@ const OPTIONS =
 
     // Matrix & Transformations
     const myMatrix = mat4.create(); // create identity matrix
+    const transMatrix = mat4.create(); 
+    const bodyScale = mat4.create();
+    let finalMatrix = mat4.create();
+
+    // code: translate->scale->rotate
+    // actually performed: rotate->scale->translate
 
     // transformations are done in reverse order of code
-    mat4.scale(myMatrix, myMatrix, [0.25, 0.25, 0.25]);
+    mat4.translate(myMatrix, myMatrix, [0,0,-1]);
+    const scaleFactor = 0.25;
+    mat4.scale(myMatrix, myMatrix, [scaleFactor, scaleFactor, scaleFactor]);
+    mat4.scale(bodyScale, bodyScale, [1, 2, 1]);
 
     const isoMatrix = mat4.create();
     if (OPTIONS.Isometric_View)
     {
-        mat4.rotateZ(isoMatrix, isoMatrix, Math.PI/4);
-        mat4.rotateY(isoMatrix, isoMatrix, -Math.PI/4);
-        mat4.rotateX(isoMatrix, isoMatrix, Math.PI/4);            
+        // true iso view is:
+        // rotate X 45 degrees (pi/4 radians)
+        // rotate Y approx. 35.3644 degrees (approx. 0.617225218 radians)
+        // pi * 0.2 = approx. 0.62831853071 radians
+        // pi * 0.2 is used
+        mat4.rotateX(isoMatrix, isoMatrix, Math.PI/4);
+        mat4.rotateY(isoMatrix, isoMatrix, Math.PI * 0.2);         
     }
 
-    let transMatrix = mat4.create(); 
+    const viewMatrix = mat4.create();
+    mat4.translate(viewMatrix, viewMatrix, [-1, 2, 0.5]);
+    mat4.invert(viewMatrix, viewMatrix);
 
-    let rectScale = mat4.create();
+    const perspectiveMatrix = mat4.create();
+    mat4.perspective(perspectiveMatrix,
+        120 * Math.PI/180,// Vertical FOV (angle, radians)
+        canvas.width/canvas.height, // aspect W/H
+        1e-4, // near cull distance/plane
+        1e4, // far cull distance/plane
+        );
+
+    const orthoMatrix = mat4.create();
+    mat4.ortho(orthoMatrix, -1, 1, -1, 1, 1e-4, 1e4);
+    mat4.multiply(orthoMatrix, orthoMatrix, isoMatrix);
 
     const uniformLocations =
     {
-        matrix: gl.getUniformLocation(program, "u_GLSLmatrix"),
-        transMatrix: gl.getUniformLocation(program, "u_transMatrix"),
-        isoMatrix: gl.getUniformLocation(program, "u_isoMatrix"),
+        u_modelMatrix: gl.getUniformLocation(program, "u_GLSLmatrix"),
+        u_transMatrix: gl.getUniformLocation(program, "u_transMatrix"),
+        u_projectionMatrix: gl.getUniformLocation(program, "u_projectionMatrix"),
+        u_viewMatrix: gl.getUniformLocation(program, "u_viewMatrix")
         
+    };
+
+    const attribSettings =
+    {
+        // attrib pointer vars
+        // draw vars
+        primitiveType: gl.TRIANGLES,
+        vSize: 3,          // 3 components per iteration
+        cSize: 4,          // 4 components per iteration
+        type: gl.FLOAT,    // the data is 32bit floats
+        indexType: gl.UNSIGNED_SHORT, // 2 byte shorts
+        normalize: false,  // don't normalize the data
+        stride: 0,         // 0 = move forward size * sizeof(type) each iteration to get the next position
+        offset: 0,         // start at the beginning of the buffer
+        indexOffset: 0,    // same as offset but for draw elements
+
+        //var count = vertexData.length / 3;
+        count: 36, // 6 verticies per face * 6 faces
     };
 
     render();
@@ -197,11 +260,13 @@ const OPTIONS =
     function render()
     {
         // Animate
-        requestAnimationFrame(render); // parameter is name of function it's in
+        if (OPTIONS.Animate)
+            requestAnimationFrame(render); // parameter is name of function it's in
 
         /*
         *   [Resizing Canvas code would go here]
         */
+        //resizeCanvasToDisplaySize(canvas);
 
         // Map clipboard coords to canvas size
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -212,77 +277,59 @@ const OPTIONS =
 
         // Execute WebGL Program
         gl.useProgram(program);
-
-        // attrib pointer vars
-        let vSize = 3;          // 3 components per iteration
-        let cSize = 4;          // 4 components per iteration
-        let type = gl.FLOAT;   // the data is 32bit floats
-        let normalize = false; // don't normalize the data
-        let stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-        let offset = 0;        // start at the beginning of the buffer
-        
+  
         // Matrix Locations
-        gl.uniformMatrix4fv(uniformLocations.isoMatrix, false, isoMatrix);
+        //gl.uniformMatrix4fv(uniformLocations.isoMatrix, false, isoMatrix);
         if (OPTIONS.Rotating)
         {
             mat4.rotateZ(myMatrix, myMatrix, Math.PI/2 /90);
             mat4.rotateX(myMatrix, myMatrix, Math.PI/2 /70);    
         }
 
-        gl.uniformMatrix4fv(uniformLocations.matrix, false, myMatrix);
-        transMatrix = mat4.create();
-        gl.uniformMatrix4fv(uniformLocations.transMatrix, false, transMatrix);
+        gl.uniformMatrix4fv(uniformLocations.u_projectionMatrix, false, orthoMatrix);
+        gl.uniformMatrix4fv(uniformLocations.u_viewMatrix, false, viewMatrix);
 
         // Ask WebGL to execute GLSL program
-        let primitiveType = gl.TRIANGLES;
-        let drawOffset = 0;
-        //var count = vertexData.length / 3;
-        let count = 36 // 6 verticies per face * 6 faces
-        let indexType = gl.UNSIGNED_SHORT;
 
         gl.enable(gl.DEPTH_TEST);
-        //gl.enable(gl.CULL_FACE);
+        gl.enable(gl.CULL_FACE);
 
-        // position
+        // position (Verticies for Cubes)
         myWebGL.attribEnableBind(gl, attribs.a_position, attribBuffers.positionCube);
-        // bind the buffer containing the indices
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-        // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-        gl.vertexAttribPointer(attribs.a_position, 
-            vSize, type, normalize, stride, offset);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer); // bind the buffer containing the indices
+        myWebGL.vertexAttribPointerV(gl, attribs.a_position, attribSettings); // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
 
         // center
         // color
         myWebGL.attribEnableBind(gl, attribs.a_color, attribBuffers.color0);
-        gl.vertexAttribPointer(attribs.a_color,
-            cSize, type, normalize, stride, offset);
+        myWebGL.vertexAttribPointerC(gl, attribs.a_color, attribSettings);
 
-        transMatrix = mat4.create();
-        rectScale = mat4.create();
-        mat4.scale(rectScale, transMatrix, [1, 1, 2]);
+        mat4.multiply(finalMatrix, bodyScale, myMatrix);
+        gl.uniformMatrix4fv(uniformLocations.u_modelMatrix, false, finalMatrix);
 
-        gl.uniformMatrix4fv(uniformLocations.transMatrix, false, rectScale);
-        gl.drawElements(primitiveType, count, indexType, drawOffset);
+        myWebGL.drawElements(gl, attribSettings);
 
-        // left 
-        myWebGL.attribEnableBind(gl, attribs.a_color, attribBuffers.colorGreen);
-        gl.vertexAttribPointer(attribs.a_color,
-            cSize, type, normalize, stride, offset);
-            
-        transMatrix = mat4.create();
-        mat4.translate(transMatrix, transMatrix, [-0.55, 0, 0]);
-        gl.uniformMatrix4fv(uniformLocations.transMatrix, false, transMatrix);
-        gl.drawElements(primitiveType, count, indexType, 0);
+        // left
+        myWebGL.attribEnableBind(gl, attribs.a_color, attribBuffers.color0);
+        myWebGL.vertexAttribPointerC(gl, attribs.a_color, attribSettings);
+
+        finalMatrix = mat4.create();
+        mat4.translate(finalMatrix, finalMatrix, [-2.1,0,0]);
+        mat4.multiply(finalMatrix, myMatrix, finalMatrix); //scale down
+        gl.uniformMatrix4fv(uniformLocations.u_modelMatrix, false, finalMatrix);
+
+        myWebGL.drawElements(gl, attribSettings);
 
         // right 
-        myWebGL.attribEnableBind(gl, attribs.a_color, attribBuffers.colorPurple);
-        gl.vertexAttribPointer(attribs.a_color,
-            cSize, type, normalize, stride, offset);
+        myWebGL.attribEnableBind(gl, attribs.a_color, attribBuffers.color0);
+        myWebGL.vertexAttribPointerC(gl, attribs.a_color, attribSettings);
 
-        transMatrix = mat4.create();
-        mat4.translate(transMatrix, transMatrix, [0.55, 0, 0]);
-        gl.uniformMatrix4fv(uniformLocations.transMatrix, false, transMatrix);
-        gl.drawElements(primitiveType, count, indexType, 0);
+        finalMatrix = mat4.create();
+        mat4.translate(finalMatrix, finalMatrix, [2.1,0,0]);
+        mat4.multiply(finalMatrix, myMatrix, finalMatrix); //scale down
+        gl.uniformMatrix4fv(uniformLocations.u_modelMatrix, false, finalMatrix);
 
+
+        myWebGL.drawElements(gl, attribSettings);
     };
 }
